a3f1fbb31f57442a68e4a24afd19247eb385c0de
kromate <akpananthony33@gmail.com>
2020-05-06 03:20:19 +0100

    pwa2

--- sw.js
+++ sw.js
@@ -1,4 +1,6 @@
 const staticCacheName = 'site-static-v1';
+const dynamicCacheName = 'site-dynamic-v1';
+
 const asset = [
   '/',
   '/index.html',
@@ -12,22 +14,37 @@ const asset = [
   '/js/initialize.js',
   '/js/script.js',
   '/style/theme.min.css',
-  'https://platform-api.sharethis.com/js/sharethis.js#property=5eab29747525e90012616b88&product=inline-share-buttons',
+  'https://cors-anywhere.herokuapp.com/https://platform-api.sharethis.com/js/sharethis.js#property=5eab29747525e90012616b88&product=inline-share-buttons',
   'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js',
-  'https://use.fontawesome.com/7672e5925f.js',
+  'https://cors-anywhere.herokuapp.com/https://use.fontawesome.com/7672e5925f.js',
 ];
 // install service worker
 
 self.addEventListener('install', (evt) => {
   // console.log('service worker has been installed' )
-  console.log('caching stuff')
+
+  console.log(evt);
   evt.waitUntil(
-    caches.open(staticCacheName).then((cache) => {
+    caches.open(staticCacheName).then(cache => {
+      console.log('caching stuff');
       cache.addAll(asset)
+    }).catch((err)=>{
+      console.log(err)
     })
   );
 
 })
+
+// cache size limit function
+const limitCacheSize = (name, size) => {
+  caches.open(name).then(cache => {
+    cache.keys().then(keys => {
+      if (keys.length > size) {
+        cache.delete(keys[0]).then(limitCacheSize(name, size));
+      }
+    });
+  });
+};
 
 // activate event
 
@@ -37,7 +54,7 @@ self.addEventListener('activate', (evt) => {
     caches.keys().then(keys => {
       //console.log(keys);
       return Promise.all(keys
-        .filter(key => key !== staticCacheName)
+        .filter(key => key !== staticCacheName && key !== dynamicCacheName)
         .map(key => caches.delete(key))
       );
     })
@@ -46,13 +63,22 @@ self.addEventListener('activate', (evt) => {
 })
 
 // fetch event
-
-self.addEventListener('fetch', (evt) => {
-  // console.log('service worker has been activated', evt)
-
+self.addEventListener('fetch', evt => {
+  //console.log('fetch event', evt);
   evt.respondWith(
-    caches.match(evt.request).then(cacheRes =>{
-      return cacheRes || fetch(evt.request)
+    caches.match(evt.request).then(cacheRes => {
+      return cacheRes || fetch(evt.request).then(fetchRes => {
+        return caches.open(dynamicCacheName).then(cache => {
+          cache.put(evt.request.url, fetchRes.clone());
+          // check cached items size
+          limitCacheSize(dynamicCacheName, 15);
+          return fetchRes;
+        })
+      });
+    }).catch(() => {
+      if (evt.request.url.indexOf('.html') > -1) {
+        return caches.match('/pages/fallback.html');
+      }
     })
-  )
-})
+  );
+});
