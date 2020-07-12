805f800a945a60a0ef1c3541bbe00ebf5583074e
kromate <akpananthony33@gmail.com>
2020-07-12 01:28:17 +0100

    -

--- js/filter.js
+++ js/filter.js
@@ -1,8 +1,10 @@
 // Initialize Cloud Firestore through Firebase
 var db = firebase.firestore();
 var booksRef = db.collection(`books`);
+var table = document.querySelector('.tbodyData')
 
-$(document).ready(function () {
+
+const search = function () {
     const dropDown = $('#dropDown').val();
     $(`#dropDown a`).click(function (e) {
         e.preventDefault(); // cancel the link behaviour
@@ -11,6 +13,7 @@ $(document).ready(function () {
 
         // console.log(selText)
         $(`#searchArchive`).keyup(function () {
+            table.innerHTML = ''
             // console.log('You entered: ', $(this).val());
             //Get the Employee Data
             if (selText == 'Uploader') {
@@ -57,21 +60,34 @@ $(document).ready(function () {
 
 
     function LoadTableData(querySnapshot) {
-        var tableRow = '';
+        var storageReference = firebase.storage().ref();
         querySnapshot.forEach(function (doc) {
             var document = doc.data();
-            tableRow += '<tr style="height: 1rem;">';
-            tableRow += '<td class="book">' + `<a href=${document.book}; download=${document.bookName}><i class="fa fa-arrow-down"  style="color:green"></i> </a>` + '</td>'
-            tableRow += '<td class="bookName">' + encodeURIComponent(document.bookName) + '</td>';
-            tableRow += '<td class="bookAuthor">' + encodeURIComponent(document.bookAuthor) + '</td>';
-            tableRow += '<td class="university">' + encodeURIComponent(document.university) + '</td>';
-            tableRow += '<td class="faculty">' + encodeURIComponent(document.faculty) + '</td>';
-            tableRow += '<td class="department">' + encodeURIComponent(document.department) + '</td>';
-            tableRow += '<td class="level">' + encodeURIComponent(document.level) + '</td>';
-            tableRow += '<td class="semester">' + encodeURIComponent(document.semester) + '</td>';
-            tableRow += '<td class="uploader">' + encodeURIComponent(document.uploader) + '</td>';
-            tableRow += '</tr>';
-        });
-        $('tbody.tbodyData').html(tableRow.split("%20").join(" ").split("%26").join("&").split("%2C").join(","));
-    }
-});
+            // console.log(document)
+            storageReference
+                .child("books/" + `${document.book}`)
+                .getDownloadURL()
+                .then(url => {
+                    // console.log(url)
+
+
+                    var tableRow = ``;
+                    tableRow += `<tr style="height: 1rem;">`;
+                    tableRow += `<td class="book"><a href=${url}; download=${document.bookName}><i class="fa fa-arrow-down"  style="color:green"></i> </a></td>`
+                    tableRow += `<td class="bookName"> ${document.bookName}</td>`;
+                    tableRow += `<td class="bookAuthor"> ${document.bookAuthor}</td>`;
+                    tableRow += `<td class="university"> ${document.university}</td>`;
+                    tableRow += `<td class="faculty"> ${document.faculty} </td>`;
+                    tableRow += `<td class="department"> ${document.department} </td>`;
+                    tableRow += `<td class="level"> ${document.level} </td>`;
+                    tableRow += `<td class="semester"> ${document.semester} </td>`;
+                    tableRow += `<td class="uploader"> ${document.uploader}</td>`;
+                    tableRow += `</tr>`;
+                    table.innerHTML += tableRow;
+                })
+        })
+    };
+}
+
+
+search()
--- js/initialize.js
+++ js/initialize.js
@@ -1,52 +1,11 @@
 // Initialize Cloud Firestore through Firebase
 var db = firebase.firestore();
 var booksRef = db.collection("books");
-
-
-
-//  booksRef.doc("1").set({
-//      uploader: "Harith",
-//      university: "university of lagos",
-//      bookName: "stuff",
-//      bookAuthor: "Ben 10",
-//      faculty: 'Engineering',
-//      department: "systems",
-//      level: 200,
-//      semester: "first",
-//      book: "jojo.pdf"
-//  });
-
-//  booksRef.doc("2").set({
-//      uploader: "Heritage",
-//      university: "Unversity of Ibadan",
-//      bookName: "I hate school",
-//      bookAuthor: "hard guy",
-//      faculty: 'Engineering',
-//      department: "chemical",
-//      level: 200,
-//      semester: "first",
-//      book: "jojo.pdf"
-//  });
-
-//  booksRef.doc("3").set({
-//      uploader: "Deji",
-//      university: "university of abuja",
-//      bookName: "Their Daddy",
-//      bookAuthor: "Naruto uzumaki",
-//      faculty: 'Engineering',
-//      department: "met&mat",
-//      level: 200,
-//      semester: "first",
-//      book: "jojo.pdf"
-//  });
-
-
-
+var table = document.querySelector('.tbodyData')
 
 
 
 booksRef.get().then(function (querySnapshot) {
-  // console.log("SHOW-FILE called!");
 
   var storageReference = firebase.storage().ref();
   querySnapshot.forEach(function (doc) {
@@ -56,25 +15,25 @@ booksRef.get().then(function (querySnapshot) {
     .child("books/" + `${document.book}`)
     .getDownloadURL()
     .then(url => {
+      // console.log(url)
 
 
-        var tableRow = '';
-      tableRow += '<tr style="height: 1rem;">';
-      tableRow += '<td class="book">' + `<a href=${document.book}; download=${document.bookName}><i class="fa fa-arrow-down"  style="color:green"></i> </a>` + '</td>'
-      tableRow += '<td class="bookName">' + encodeURIComponent(document.bookName) + '</td>';
-      tableRow += '<td class="bookAuthor">' + encodeURIComponent(document.bookAuthor) + '</td>';
-      tableRow += '<td class="university">' + encodeURIComponent(document.university) + '</td>';
-      tableRow += '<td class="faculty">' + encodeURIComponent(document.faculty) + '</td>';
-      tableRow += '<td class="department">' + encodeURIComponent(document.department) + '</td>';
-      tableRow += '<td class="level">' + encodeURIComponent(document.level) + '</td>';
-      tableRow += '<td class="semester">' + encodeURIComponent(document.semester) + '</td>';
-      tableRow += '<td class="uploader">' + encodeURIComponent(document.uploader) + '</td>';
-      tableRow += '</tr>';
-        $('tbody.tbodyData').append(tableRow.split("%20").join(" ").split("%26").join("&").split("%2C").join(","));
+        var tableRow = ``;
+      tableRow += `<tr style="height: 1rem;">`;
+      tableRow += `<td class="book"><a href=${url}; download=${document.bookName}><i class="fa fa-arrow-down"  style="color:green"></i> </a></td>`
+      tableRow += `<td class="bookName"> ${ document.bookName }</td>`;
+      tableRow += `<td class="bookAuthor"> ${document.bookAuthor}</td>`;
+      tableRow += `<td class="university"> ${document.university}</td>`;
+      tableRow += `<td class="faculty"> ${document.faculty} </td>`;
+      tableRow += `<td class="department"> ${document.department} </td>`;
+      tableRow += `<td class="level"> ${document.level} </td>`;
+      tableRow += `<td class="semester"> ${document.semester} </td>`;
+      tableRow += `<td class="uploader"> ${document.uploader}</td>`;
+      tableRow += `</tr>`;
+       table.innerHTML +=tableRow;
 
     }).catch(error => {
-      // alert("Something went wrong!");
-      // console.log("Error === ", error);
+      console.log(error)
     });
     })
 
